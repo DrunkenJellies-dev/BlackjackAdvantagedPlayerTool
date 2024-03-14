d5686626d4ec931b9a8758b8c40081509d486732
CallumNewman <ballumnew@gmail.com>
2024-03-14 22:12:03 +0000

    Added value setting to the card class based on the rank of the card

--- run.py
+++ run.py
@@ -10,9 +10,27 @@ class Card():
         self.rank = rank
         self.suit = suit
 
+    #represents the card in a string
     def __repr__(self):
         return f"{self.rank} of {self.suit}"
+    
+    @property
+    def value(self):
+        #Aces have the initial value of 11 
+        if self.rank == "Ace":
+            value = 11
+        else:
+            try:
+                #value of the card equals the rank
+                value = int(self.rank)
+            except:
+                #if unable to convert rank to int for face cards then the value is 10
+                value = 10
+        return value
+    
+
+
 
 
 card = Card("8", "Diamonds")
-print(card.rank + " " + card.suit)
+print(card.value)
