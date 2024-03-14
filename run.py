1e278198fabf7bbc7688f57d643d3657bdb93bf8
CallumNewman <ballumnew@gmail.com>
2024-03-14 22:02:26 +0000

    Added card class

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+class Card():
+    """
+    Class to represent card values that can be used when playing to determine the value of the hand
+    """
+
+    #initializing card rank (e.g. King) and suit (e.g. Hearts)
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __repr__(self):
+        return f"{self.rank} of {self.suit}"
+
+
+card = Card("8", "Diamonds")
+print(card.rank + " " + card.suit)
